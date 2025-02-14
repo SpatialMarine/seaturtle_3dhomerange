42a6a4f412f2449aa2d381ce35e35806bfaa753a
jmenblaz <javier.menendezruv.es>
2025-02-14 19:55:48 +0100

    update

--- fig/01_3d/01_3d_track.R
+++ fig/01_3d/01_3d_track.R
@@ -9,7 +9,7 @@
 
 
 ## Load libraries
-
+library(sf)
 library(plot3D)
 library(rgl)
 library(plot3Drgl)
@@ -22,17 +22,39 @@ library(ncdf4)
 library(rayshader)
 
 
-
-# plot diorama  --------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# 1) Prepare data for plot diorama      ---------------------------------
 
 # limit for represent
 xlim <- c(0.6, 4.7)
 ylim <- c(36.9, 40.1)
 
+# landmask world <- giscoR::gisco_get_countries(year = "2016", epsg = "4326", resolution = "03")
+land <- giscoR::gisco_get_countries(year = "2016", epsg = "4326", resolution = "03")
+
+# bounding box
+# Definir el bounding box (asegurándonos que el CRS sea el adecuado)
+bb <- st_as_sfc(st_bbox(c(xmin = xlim[1], xmax = xlim[2], 
+                          ymin = ylim[1], ymax = ylim[2])), 
+                crs = st_crs(land)) 
+# crop
+land <- st_crop(land, bb)
+plot(land$geometry)
+
+
 
 # import location data for plot
 organismID <- "200043"
+# read ttdr data
 ttdr <- read.csv(paste0(input_dir,"/tracking/ttdr/L3/",organismID,"_L3_ttdr.csv"))
+
+# first and last position
+start <- head(ttdr, 1)
+end  <- tail(ttdr, 1)
+# latitude and longitude coordinates for positions
+start <- c(start$latitude, start$longitude)
+end <- c(end$latitude, end$longitude)
+
 
 # import bathymetry GEBCO 2024
 # note bathymetry cropped to mediterranean sea
@@ -41,17 +63,20 @@ ttdr <- read.csv(paste0(input_dir,"/tracking/ttdr/L3/",organismID,"_L3_ttdr.csv"
 b <- raster(paste0(input_dir, "/gis/gebco/mediterranean_sea_gebco_2024.tif"))
 
 # use only values == or < 0 as a bathymetry
-# b[b > 0] <- NA #0 data as NA
+b[b > 0] <- 50  # surface values as 10 m (flat land mask)
 
 # Obtain values of batyhymetry in the plot area xlim and ylim
 b <- crop(b, extent(c(xlim, ylim)))
 plot(b) # check plotting area
 
-# bath_3035 <- projectRaster(bath, crs = CRS("+init=epsg:3035"))
-# elevation.raster <- bath_3035
 
 
-# Prepare diorama -----------------------------------------------
+
+
+
+
+# -----------------------------------------------------------------------------
+# 2) Prepare diorama              -----------------------------------------
 
 # rename object
 elevation.raster <- b
@@ -60,15 +85,18 @@ elevation.raster <- b
 elevation.matrix <- matrix(extract(elevation.raster, extent(elevation.raster), buffer = 100), nrow = ncol(elevation.raster), ncol = nrow(elevation.raster))
 
 # Z scale for elevation matrix
-my.z <- 50
+my.z <- 25
 
+plot(elevation.matrix)
 # elevantion.matrix %>%
 #   sphere_shade(zscale= my.z,
 #                texture=create_texture("#E9C68D","#AF7F38",
 #                                       "#674F30","#494D30",
 #                                        "#B3BEA3")) %>%
 #   plot_map()
-# 
+
+
+  
 
 # test map
 elevation.matrix  %>% 
@@ -77,15 +105,16 @@ elevation.matrix  %>%
 
 # ambient occlusion
 elevation.amb.shade <- ambient_shade(elevation.matrix, zscale = my.z, multicore = TRUE, progbar = TRUE)
-plot_map(elevation.amb.shade)
+# plot_map(elevation.amb.shade)
 
 # ray shadow
 elevation.ray.shade <- ray_shade(elevation.matrix,sunangle = 35, zscale = my.z, multicore = TRUE)
-plot_map(elevation.ray.shade)
+# plot_map(elevation.ray.shade)
 
-rm(elevation.amb.shade, elevation.matrix)
 
-# plot diorama ----------------------
+
+
+# plot diorama -----------------------------------------------------------------
 
 # plot diorama
 elevation.matrix  %>% 
@@ -103,22 +132,12 @@ elevation.matrix  %>%
 
 # add effects to ploted diorama
 render_water(elevation.matrix, zscale = my.z, 
-             wateralpha = 0.2, 
+             wateralpha = 0.22, 
              watercolor = "skyblue1",
-             waterlinealpha = 0.7,
+             waterlinealpha = 0.75,
              waterlinecolor = "lightblue1",
              remove_water = TRUE)
 
-
-
-# 
-# render_points(extent = attr(elevation.raster,"extent"), 
-#               lat = unlist(ttdr$latitude), long = unlist(ttdr$longitude),
-#               altitude = (ttdr$depth)*-1,
-#               offset = -1,
-#               zscale = my.z,
-#               size = 6,
-#               color="black")
 
 # render path / track of organism ID
 render_path(extent = attr(elevation.raster,"extent"), 
@@ -130,13 +149,36 @@ render_path(extent = attr(elevation.raster,"extent"),
             # size = 4,
             color="black")
 
+
 # add labels
-# render_label(heightmap = elevation.matrix, x = 797, y = 963-509, z = 10000, linewidth = 1, zscale = my.z, text = "Hilo")
-# render_label(heightmap = elevation.matrix, x = 90, y = 963-304, z = 10000, linewidth = 1, zscale = my.z, text = "Mauna Kea - 4205m")
+# start
+render_label(elevation.matrix, lat = start[1], long = start[2], 
+             z = 10,
+             altitude = 1000,
+             extent = attr(elevation.raster, "extent"),
+             zscale = my.z, 
+             text = "Start", textcolor = "black", linecolor="darkgreen",
+             dashed = FALSE)
+# end
+render_label(elevation.matrix, lat = end[1], long = end[2],
+             z = 5000,
+             altitude = 1000, 
+             extent = attr(elevation.raster, "extent"),
+             zscale = my.z, 
+             text = "End", textcolor = "black", linecolor="darkred",
+             dashed = FALSE)
 
 
-# render_snapshot("mallorca_render.png")
-# render_snapshot()
+# add countries
+render_polygons(land, 
+                extent = attr(elevation.raster,"extent"),
+                top = 50,
+                parallel = TRUE)
+
+# extent_latlong = sp::SpatialPoints(rbind(bottom_left, top_right), 
+#                                          proj4string=sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+# attr(elevation.matrix, "extent") = extent_latlong
+
 
 
 
@@ -144,24 +186,20 @@ render_path(extent = attr(elevation.raster,"extent"),
 render_camera(theta = 315, phi = 25, zoom = 0.5, fov = 15)
 
 
-# export / save image
+
+
+# export / save diorama --------------------------------------------------------
+# render snapshot
+render_snapshot(filename = paste0(output_dir, "/fig/fig_track_3d.png"))
+
+
+
 # note** high comsuming time process
 # highquality
 # render_snapshot(filename = paste0(output_dir, "/fig/fig_track_3d.png"),
 #                 software_render = TRUE)
 # 
 # render_highquality(filename = paste0(output_dir, "/fig/fig_track_3d_hd.png"))
-
-# render 
-render_snapshot(filename = paste0(output_dir, "/fig/fig_track_3d.png"))
-
-
-
-
-
-
-
-
 
 
 
